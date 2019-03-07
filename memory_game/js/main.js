577682bdc65d2d77c04c0672dcf05ddea9e03c93
Rickey Weems <rweems@rickeys-mbp.attlocal.net>
2019-03-07 16:22:10 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+console.log("User has flipped " + cardTwo);
+console.log("User has flipped " + cardThree);
