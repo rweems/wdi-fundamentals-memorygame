c94c67eb071182a6d4ab6cfca86d13a8b8fefc0a
Rickey Weems <rweems@rickeys-mbp.attlocal.net>
2019-03-08 13:14:17 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,22 @@
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
-console.log("User has flipped " + cardTwo);
-console.log("User has flipped " + cardThree);
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardTwo);
+
+if(cardsInPlay.length === 2)
+{
+	if(cardsInPlay[0] === cardsInPlay[1])
+	{
+		alert("You found a match!");
+	}
+	else
+	{
+		alert("Sorry, try again!");
+	}
+}
