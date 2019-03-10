f6de9d87cf4347050fa83c104afa433c236c3cfc
Rickey Weems <rweems@rickeys-mbp.attlocal.net>
2019-03-10 13:29:08 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,7 @@
 
 
 
+
 var cards = [
 {
 	rank: "queen",
@@ -28,30 +29,44 @@ var cardsInPlay = [];
 
 var checkForMatch = function()
 {
-	if(cardsInPlay.length === 2)
+	
+	
+	if(cardsInPlay[0] === cardsInPlay[1])
 	{
-		if(cardsInPlay[0] === cardsInPlay[1])
-		{
-			alert("You found a match!");
-		}
-		else
-		{
-			alert("Sorry, try again!");
-		}
+		alert("You found a match!");
 	}
+	else
+	{
+		alert("Sorry, try again!");
+	}
+
 }
 
-var flipCard = function(cardId)
+var flipCard = function()
 {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	checkForMatch();
+	this.setAttribute('src', cards[cardId].cardImage);
+	if(cardsInPlay.length === 2)
+		checkForMatch();
 
 
 }
 
+var createBoard = function()
+{
+	for(var i = 0; i < cards.length; i++)
+	{
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
 
-flipCard(0);
-flipCard(2);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
